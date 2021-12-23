c5de77a3fc587830c54b4d3748cc414a7e05b64b
ImanJofaris <imanjofaris@gmail.com>
2021-12-24 05:54:17 +0800

    dashboard user interface

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -31,4 +31,10 @@ class HomeController extends Controller
             return redirect()->back();
         }
     }
+
+    public function index()
+    {
+        return view('user.home');
+
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
+
+Route::get('/','App\Http\Controllers\HomeController@index');
 //determine the user
 Route::get('/home','App\Http\Controllers\HomeController@redirect');
 
