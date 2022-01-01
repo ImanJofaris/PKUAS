a408f4245e62ae77153e0a311ec1532f7ac2e319
ImanJofaris <imanjofaris@gmail.com>
2022-01-01 18:07:39 +0800

    fix href on admin sidebar

--- resources/views/admin/sidebar.blade.php
+++ resources/views/admin/sidebar.blade.php
@@ -19,7 +19,7 @@
 
         <!-- Nav Item - Dashboard -->
         <li class="nav-item active">
-            <a class="nav-link" href={{ url('admin_home') }}>
+            <a class="nav-link" href={{ url('home') }}>
                 <i class="fas fa-fw fa-tachometer-alt"></i>
                 <span>Dashboard</span></a>
         </li>
